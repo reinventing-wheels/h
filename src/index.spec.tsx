2c7da13057794b2e9001cd4a354c8163c3057ba9
foo <5144598+bar@users.noreply.github.com>
2018-09-30 11:42:01 +0300

    tests: some changes

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -1,71 +1,59 @@
 import { Component, React, render } from '.'
 
-describe('h', () => {
-  const unsafe = '<>&"'
-  const safe = '&#60;&#62;&#38;&#34;'
+const unsafe = '<>&"'
+const safe = '&#60;&#62;&#38;&#34;'
 
-  it('should insert raw html', () => {
-    const vdom = <p html={unsafe} />
-    const html = `<p>${unsafe}</p>`
-    expect(render(vdom)).toBe(html)
-  })
+it('should insert raw html', () => {
+  const vdom = <p html={unsafe} />
+  const html = `<p>${unsafe}</p>`
+  expect(render(vdom)).toBe(html)
+})
 
-  it('should escape unsafe input', () => {
-    const vdom = <p id={unsafe}>{unsafe}</p>
-    const html = `<p id="${safe}">${safe}</p>`
-    expect(render(vdom)).toBe(html)
-  })
+it('should encode html entities', () => {
+  const vdom = <p id={unsafe}>{unsafe}</p>
+  const html = `<p id="${safe}">${safe}</p>`
+  expect(render(vdom)).toBe(html)
+})
 
-  it('should support nesting', () => {
-    expect(render(<p></p>)).toBe('<p></p>')
-    expect(render(<p>foo</p>)).toBe('<p>foo</p>')
-    expect(render(<p>{''}</p>)).toBe('<p></p>')
-    expect(render(<p>{'foo'}</p>)).toBe('<p>foo</p>')
-    expect(render(<p>{['f', ['o', ['o']]]}</p>)).toBe('<p>foo</p>')
-    expect(render(<p>{0}</p>)).toBe('<p>0</p>')
-    expect(render(<p>{1}</p>)).toBe('<p>1</p>')
-    expect(render(<p>{null}</p>)).toBe('<p></p>')
-    expect(render(<p>{undefined}</p>)).toBe('<p></p>')
-    expect(render(<p>{true}</p>)).toBe('<p></p>')
-    expect(render(<p>{false}</p>)).toBe('<p></p>')
-  })
+it('should support nesting', () => {
+  expect(render(<p></p>)).toBe('<p></p>')
+  expect(render(<p>{''}</p>)).toBe('<p></p>')
+  expect(render(<p>{null}</p>)).toBe('<p></p>')
+  expect(render(<p>{undefined}</p>)).toBe('<p></p>')
+  expect(render(<p>{true}</p>)).toBe('<p></p>')
+  expect(render(<p>{false}</p>)).toBe('<p></p>')
+  expect(render(<p>{0}</p>)).toBe('<p>0</p>')
+  expect(render(<p>{1}</p>)).toBe('<p>1</p>')
+  expect(render(<p>foo</p>)).toBe('<p>foo</p>')
+  expect(render(<p>{'foo'}</p>)).toBe('<p>foo</p>')
+  expect(render(<p>{['f', ['o', ['o']]]}</p>)).toBe('<p>foo</p>')
+})
 
-  it('should support attributes', () => {
-    expect(render(<input value='' />)).toBe('<input value>')
-    expect(render(<input value='foo' />)).toBe('<input value="foo">')
-    expect(render(<input value={0} />)).toBe('<input value="0">')
-    expect(render(<input value={1} />)).toBe('<input value="1">')
-    expect(render(<input value={null} />)).toBe('<input>')
-    expect(render(<input value={undefined} />)).toBe('<input>')
-    expect(render(<input disabled />)).toBe('<input disabled>')
-    expect(render(<input disabled={true} />)).toBe('<input disabled>')
-    expect(render(<input disabled={false} />)).toBe('<input>')
-  })
+it('should support attributes', () => {
+  expect(render(<input value='' />)).toBe('<input value>')
+  expect(render(<input value={null} />)).toBe('<input>')
+  expect(render(<input value={undefined} />)).toBe('<input>')
+  expect(render(<input value={0} />)).toBe('<input value="0">')
+  expect(render(<input value={1} />)).toBe('<input value="1">')
+  expect(render(<input value='foo' />)).toBe('<input value="foo">')
+  expect(render(<input disabled />)).toBe('<input disabled>')
+  expect(render(<input disabled={true} />)).toBe('<input disabled>')
+  expect(render(<input disabled={false} />)).toBe('<input>')
+})
 
-  it('should support fragments', () => {
-    expect(render(<></>)).toBe('')
-    expect(render(<>foo</>)).toBe('foo')
-    expect(render(<>foo<br/>bar</>)).toBe('foo<br>bar')
-    expect(render(<><br/><br/></>)).toBe('<br><br>')
-  })
+it('should support fragments', () => {
+  expect(render(<></>)).toBe('')
+  expect(render(<>foo</>)).toBe('foo')
+  expect(render(<>foo<br/>bar</>)).toBe('foo<br>bar')
+  expect(render(<><br/><br/></>)).toBe('<br><br>')
+})
 
-  it('should support components', () => {
-    const Head: Component<{ title: string }> = props => <>
-      <title>{ props.title }</title>
-    </>
+it('should support components', () => {
+  const Head: Component = p => <><title>{ p.title }</title></>
+  const Body: Component = p => <><h1>{ p.title }</h1><p>{ p.content }</p></>
+  const Page: Component = p => <><Head { ...p } /><Body { ...p } /></>
 
-    const Body: Component<{ title: string, content: string }> = props => <>
-      <h1>{ props.title }</h1>
-      <p>{ props.content }</p>
-    </>
-
-    const Page: Component<{ title: string, content: string }> = props => <>
-      <Head { ...props } />
-      <Body { ...props } />
-    </>
-
-    const vdom = <Page title='foo' content='bar' />
-    const html = '<title>foo</title><h1>foo</h1><p>bar</p>'
-    expect(render(vdom)).toBe(html)
-  })
+  const vdom = <Page title='foo' content='bar' />
+  const html = '<title>foo</title><h1>foo</h1><p>bar</p>'
+  expect(render(vdom)).toBe(html)
 })
